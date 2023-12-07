3f460beb8020ee9118227bc08faf791fc213fd1b
Dmitry Ponyatov <dponyatov@gmail.com>
2023-12-07 05:37:34 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/video.d
+++ src/video.d
@@ -25,7 +25,7 @@ TTF_Font* font = null;
 
 void ldfont() {
     TTF_Init;
-    font = TTF_OpenFont(mono, H/22);
+    font = TTF_OpenFont(mono, H / 22);
     if (font is null)
         writeln(to!string(SDL_GetError));
 }
@@ -33,16 +33,16 @@ void ldfont() {
 void drawtime() {
     SDL_Color foreground = {0x00, 0x77, 0x77};
     auto n = Clock.currTime();
-    auto now = format("%2u.%s.%4u %s %2u:%02u:%02u", n.day, n.month,
-            n.year, n.dayOfWeek, n.hour, n.minute, n.second).toStringz;
+    auto now = format("%2u.%s.%4u %s %2u:%02u:%02u", n.day,
+            n.month, n.year, n.dayOfWeek, n.hour, n.minute, n.second).toStringz;
     SDL_Surface* text_surf = TTF_RenderText_Solid(font, now, foreground);
     assert(text_surf !is null);
     SDL_Texture* text_t = SDL_CreateTextureFromSurface(renderer, text_surf);
     assert(text_t !is null);
     // 
     SDL_Rect dest;
-    dest.x = W - text_surf.w;
-    dest.y = H - text_surf.h;
+    dest.x = W / 2 - text_surf.w / 2;
+    dest.y = H / 2 - text_surf.h / 2;
     dest.w = text_surf.w;
     dest.h = text_surf.h;
     SDL_RenderCopy(renderer, text_t, null, &dest);
@@ -88,7 +88,19 @@ void mklogo() {
     logo_t = SDL_CreateTextureFromSurface(renderer, logo);
     assert(logo_t !is null);
     // 
-    logo_rect = SDL_Rect(0x11, 0x11, logo.w, logo.h);
+}
+
+int logo_x = 12, logo_y = 34, logo_dx = 2, logo_dy = 3;
+
+void mvlogo() {
+    logo_x += logo_dx;
+    if (logo_x + logo_rect.w > W || logo_x < 0)
+        logo_dx = -logo_dx;
+    logo_y += logo_dy;
+    if (logo_y + logo_rect.h > H || logo_y < 0)
+        logo_dy = -logo_dy;
+    logo_rect = SDL_Rect(logo_x, logo_y, logo.w, logo.h);
+    SDL_RenderCopy(renderer, logo_t, null, &logo_rect);
 }
 
 void fini() {
@@ -104,7 +116,7 @@ void fini() {
 void draw() {
     SDL_SetRenderDrawColor(renderer, 0x11, 0x11, 0x11, 0x00);
     SDL_RenderClear(renderer);
-    SDL_RenderCopy(renderer, logo_t, null, &logo_rect);
+    mvlogo;
     drawtime;
     SDL_RenderPresent(renderer);
 }
@@ -113,7 +125,7 @@ void loop() {
     init;
     bool quit = false;
     while (!quit) {
-        SDL_Delay(222);
+        SDL_Delay(22);
         draw;
         SDL_PumpEvents;
         while (SDL_PollEvent(&event)) {
