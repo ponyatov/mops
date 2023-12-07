6771dd6f69d0ca1b3603567a4d9c656b6ce63f7c
Dmitry Ponyatov <dponyatov@gmail.com>
2023-12-07 05:26:59 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/video.d
+++ src/video.d
@@ -2,9 +2,13 @@ module video;
 
 import std.stdio;
 import std.conv;
+import std.datetime;
+import std.string;
+import std.format;
 
 import bindbc.sdl;
 import bindbc.sdl.image;
+import bindbc.sdl.ttf;
 
 SDL_Window* window = null;
 SDL_Renderer* renderer = null;
@@ -16,6 +20,38 @@ int W = 0, H = 0;
 
 SDL_Event event;
 
+const(char)* mono = "/usr/share/fonts/truetype/noto/NotoMono-Regular.ttf";
+TTF_Font* font = null;
+
+void ldfont() {
+    TTF_Init;
+    font = TTF_OpenFont(mono, H/22);
+    if (font is null)
+        writeln(to!string(SDL_GetError));
+}
+
+void drawtime() {
+    SDL_Color foreground = {0x00, 0x77, 0x77};
+    auto n = Clock.currTime();
+    auto now = format("%2u.%s.%4u %s %2u:%02u:%02u", n.day, n.month,
+            n.year, n.dayOfWeek, n.hour, n.minute, n.second).toStringz;
+    SDL_Surface* text_surf = TTF_RenderText_Solid(font, now, foreground);
+    assert(text_surf !is null);
+    SDL_Texture* text_t = SDL_CreateTextureFromSurface(renderer, text_surf);
+    assert(text_t !is null);
+    // 
+    SDL_Rect dest;
+    dest.x = W - text_surf.w;
+    dest.y = H - text_surf.h;
+    dest.w = text_surf.w;
+    dest.h = text_surf.h;
+    SDL_RenderCopy(renderer, text_t, null, &dest);
+    // 
+    SDL_DestroyTexture(text_t);
+    SDL_FreeSurface(text_surf);
+
+}
+
 void init() {
     assert(SDL_Init(SDL_INIT_VIDEO) == 0);
     assert(SDL_GetCurrentDisplayMode(0, &display_mode) == 0);
@@ -26,6 +62,7 @@ void init() {
     winrender;
     image;
     mklogo;
+    ldfont;
 }
 
 void mouse() {
@@ -55,6 +92,7 @@ void mklogo() {
 }
 
 void fini() {
+    TTF_Quit;
     SDL_FreeSurface(logo);
     SDL_DestroyTexture(logo_t);
     IMG_Quit();
@@ -67,6 +105,7 @@ void draw() {
     SDL_SetRenderDrawColor(renderer, 0x11, 0x11, 0x11, 0x00);
     SDL_RenderClear(renderer);
     SDL_RenderCopy(renderer, logo_t, null, &logo_rect);
+    drawtime;
     SDL_RenderPresent(renderer);
 }
 
@@ -74,7 +113,7 @@ void loop() {
     init;
     bool quit = false;
     while (!quit) {
-        // SDL_Delay(222);
+        SDL_Delay(222);
         draw;
         SDL_PumpEvents;
         while (SDL_PollEvent(&event)) {
@@ -88,5 +127,4 @@ void loop() {
             }
         }
     }
-
 }
